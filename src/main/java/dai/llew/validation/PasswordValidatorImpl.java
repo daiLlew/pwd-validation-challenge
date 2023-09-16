96b4ab85481858cac18469f0af0a111b30297dc0
DaiLlew <dispositiondavey@gmail.com>
2023-09-16 15:52:45 +0100

    added some comments for reference

--- src/main/java/dai/llew/validation/PasswordValidatorImpl.java
+++ src/main/java/dai/llew/validation/PasswordValidatorImpl.java
@@ -7,12 +7,17 @@ import java.util.regex.Pattern;
  */
 public class PasswordValidatorImpl implements PasswordValidator {
 
+	// (?=) positive lookahead
+	// (?!) negative lookahead.
+	// (?=\w+\s+\w+\s+\w+\s+\w+.*)
+
 	/**
 	 * Valid passwords require:
 	 * 1 or more uppercase chars
 	 * 1 or more lowercase chars
 	 * 1 or more digits
 	 * 1 or more underscore chars.
+	 * Passwords are considered valid if they meet the above criteria even if they contain other characters.
 	 */
 	private static final String PASSWORD_REGEX = "\\A(?=.*[A-Z]+)(?=.*[a-z]+)(?=.*[0-9]+)(?=.*_+).{8,}\\Z";
 	private static final Pattern REGEX = Pattern.compile(PASSWORD_REGEX);
